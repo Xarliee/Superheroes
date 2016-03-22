842f519debe49bdd57531f1e00cbc1926c356cce
Carlos <c.salas.marin@gmail.com>
2016-03-22 19:23:16 -0300

    Segundo commit imagen de batman de fondo

---
+++ app/assets/javascripts/batman.js
@@ -0,0 +1,3 @@
+$(document).on("page:change", function(){
+  $.backstretch("http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/article_main_wide_image/public/2015/10/main.jpg?itok=Nl4CIc_f");
+  });
